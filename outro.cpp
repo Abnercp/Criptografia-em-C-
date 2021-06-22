bfaa04e02e9a8c1f8829066b21a9004fa582b206
Abner Pereira <57909294+Abnercp@users.noreply.github.com>
2021-06-21 23:01:56 -0300

    Update outro.cpp

--- outro.cpp
+++ outro.cpp
@@ -1,36 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
-#include <locale.h>
-
-int main (){
-
-    setlocale (LC_ALL, "Portuguese");
-
-    char message[50] = "";
-    char password[50] = "";
-
-    printf("Digite sua mensagem: \n");
-    scanf("%50[ˆ\n]%*c");
-
-    printf("Digite sua senha: \n");
-    scanf("%50[ˆ\n]%*c");
-
-    int messageLength = strlen(message);  // pegando o tamanho da mensagem
-
-    int randon(messageLength + 1);
-    int encrypted(messageLength + 1);
-    int decrypted(messageLength + 1);
-
-    for(int x = 0; x < messageLength; x++){
-        srand(password[x % strlen (password)])   
-        }
-        random[x] = rand();
 
 
+int main(){
+  char message[50] = "";
+  char password[50] = "";
+
+  printf("Digite uma mensagem: ");
+  scanf("%50[^\n]%*c", &message);
+
+  printf("Digite uma senha: " );
+  scanf("%50[^\n]%*c", &password);
+
+  int messageLength = strlen(message); 
+
+  char  random[messageLength + 1]; 
+  char  encrypted[messageLength + 1]; 
+  char  decrypted[messageLength + 1]; 
+
+  
+  for(int i=0; i < messageLength; i++){ 
+    srand(password[i % strlen(password)]); 
+    
+    random[i] = rand(); 
+  }
+
+  printf("Tamanho da mensagem: %d\n", messageLength);
+  
+  // Criptografando
+  for(int i=0; i < messageLength; i++){ 
+    encrypted[i] = message[i] ^ random[i]; 
+  }
+
+  printf("Codificada: %s\n", encrypted);
+
+  // Descriptografando  
+  for(int i=0; i < messageLength; i++){
+    decrypted[i] = encrypted[i] ^ random[i];
+  }
 
 
+  printf("Decodificada: %s\n", decrypted);
 
-
-    return 0;
+  return 0;
 }
